9d25c910b0b417e027a4f617005b7d9a3171753e
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2014-12-24 04:25:05 +0530

    Using grunt to optimize images

--- gruntfile.js
+++ gruntfile.js
@@ -17,13 +17,24 @@ module.exports = function(grunt) {
                 src: 'js/build/production.js',
                 dest: 'js/build/production.min.js'
             }
+        },
+        imagemin: {
+            dynamic:{
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
         }
     });
 
     // 3. Load plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // 4. Default task to run from command line
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
 };

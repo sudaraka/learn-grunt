c9ea709d70d492a6db7bb1a8d0cfb235d95570f6
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2014-12-24 03:50:33 +0530

    Using grunt to minify (uglify) JS files

--- gruntfile.js
+++ gruntfile.js
@@ -11,12 +11,19 @@ module.exports = function(grunt) {
                 ],
                 dest: 'js/build/production.js'
             }
+        },
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
         }
     });
 
     // 3. Load plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 4. Default task to run from command line
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
